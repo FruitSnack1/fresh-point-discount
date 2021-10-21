b6445edc166e15a5e972c90bd4106477010d5a73
Ryzner <tomas.ryzner@email.cz>
2021-10-21 12:12:54 +0200

    cards

--- index.js
+++ index.js
@@ -10,18 +10,39 @@ app.get('/', async (req, res) => {
     const html = data.data
     const $ = cheerio.load(html)
     const menu = []
+    const prices = []
+    const images = []
     $('.rounded-circle').each(function () {
         const item = this
+        let i = 0
+
+        $(item).parent().parent().parent().find('.text-danger').each(function (i, e) {
+            const text = $(this).text().trim()
+            prices.push(text)
+        })
+
+        $(item).parent().parent().parent().find('img').each(function (i, e) {
+            const src = $(this).attr('src')
+            images.push(src)
+        })
         $(item).parent().parent().parent().find('.font-weight-bold').each(function () {
             const text = $(this).text().trim()
             if (text != 'Poslední kus!' && !text.toString().includes('kusy')) {
-                console.log(text)
-                menu.push(text)
+                if(i%2 == 0)
+                    menu.push({name: text})
+                else
+                    menu[menu.length-1].discount = text
             }
+            i++
         })
+        for(let i = 0; i< menu.length; i++){
+            menu[i].price = prices[i]
+            menu[i].img = images[i]
+        }
+        console.log(menu)
     })
     res.render('index', { menu })
 })
 
-app.listen('80', () => console.log('Server listening...'))
+app.listen('8080', () => console.log('Server listening...'))
 
